ba6a5144b4c935ea98a6bc3187df13ddc8d895ff
sshuklaamberr <shuklaamber01@gmail.com>
2026-03-24 01:42:20 +0530

    UC2: Hardcoded Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,12 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+
+        if(str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
